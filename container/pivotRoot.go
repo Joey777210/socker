1de7533f5ddae5c8bc7e6e974852a8271a5f4af2
joey <joeyzq970519@163.com>
2020-02-23 18:59:21 -0800

    修改mount /proc 没有权限error前的备份

---
+++ container/pivotRoot.go
@@ -0,0 +1,64 @@
+package overlay2
+
+import (
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"os"
+	"path/filepath"
+	"syscall"
+)
+
+func pivotRoot(root string) error {
+	if err := syscall.Mount(root, root , "bind", syscall.MS_BIND|syscall.MS_REC, ""); err != nil {
+		return fmt.Errorf("Mount rootfs to itself error: %v", err)
+	}
+
+	//create pivotDir to store old_root
+	pivotDir := filepath.Join(root, ".pivot_root")
+	if err := os.Mkdir(pivotDir, 0777); err != nil{
+		return nil
+	}
+
+	//pivot to new rootfs
+	if err := syscall.PivotRoot(root, pivotDir); err != nil {
+		return fmt.Errorf("pivot_root %v", err)
+	}
+
+	//change to root "/" dir
+	if err := syscall.Chdir("/"); err != nil {
+		return fmt.Errorf("chdir / %v", err)
+	}
+
+
+	pivotDir = filepath.Join("/", ".pivot_root")
+	//umount rootfs/.pivot_root
+	if err := syscall.Unmount(pivotDir, syscall.MNT_DETACH); err != nil {
+		return fmt.Errorf("unmount pivot_root dir %v", err)
+	}
+
+	//delete temporary dir
+	return os.Remove(pivotDir)
+}
+
+
+/**
+	init mount rootfs
+ */
+func SetUpMount(){
+	//get current path
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Errorf("Get current location error %v", err)
+		return
+	}
+	log.Infof("Current location is %s", pwd)
+	pivotRoot(pwd)
+
+	// systemd 加入linux之后, mount namespace 就变成 shared by default, 所以你必须显示
+	//声明你要这个新的mount namespace独立。
+	syscall.Mount("", "/", "", syscall.MS_PRIVATE | syscall.MS_REC, "")
+	//mount proc
+	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+	syscall.Mount("tmpfs", "/dev", "tmpfs", syscall.MS_NOSUID | syscall.MS_STRICTATIME, "mode=755")
+}
