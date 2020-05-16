672c8b88eab39a1dbf0764482277b5bcfb0758c2
joey777210 <joeyzq970519@163.com>
2020-05-16 04:44:40 -0700

    fix Socker commit command

--- overlay2/commit.go
+++ overlay2/commit.go
@@ -10,10 +10,11 @@ func CommitContainer(imageName string){
 	mergeDirURL := fmt.Sprintf(MERGE, imageName)
 	imageTarURL := "/root" + "/" + imageName + ".tar"
 	fmt.Printf("imageTar: %s", imageTarURL)
-	cmd := "tar" + " " + "-czf" + " " + imageTarURL + " " + "-C" + " " + mergeDirURL + " " + "."
-	fmt.Printf("commit all: %s", cmd)
-	if err := exec.Command(cmd); err != nil {
-
+	//cmd := "tar" + " " + "-czPf" + " " + imageTarURL + " " + mergeDirURL
+	//cmd := "/bin/sh " + "-c " + "sudo " + "tar" + " " + "-czPf" + " " + imageTarURL + " " + mergeDirURL
+	//cmd := exec.Command("/bin/sh", "-c", "sudo " + "tar" + " " + "-czPf" + " " + imageTarURL + " " + mergeDirURL)
+	//fmt.Printf("commit all: %s", cmd)
+	if _, err := exec.Command("tar", "-czf", imageTarURL, mergeDirURL).CombinedOutput(); err != nil {
 		log.Errorf("Tar folder %s error %v", mergeDirURL, err)
 	}
 }
